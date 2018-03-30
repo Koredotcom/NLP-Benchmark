252b874b81c774891bc599da81cf6624a94f54db
Pranav <pranav.bharadwaja@kore.com>
2018-03-30 16:31:38 +0530

    added new parameters in config

--- configBot.py
+++ configBot.py
@@ -3,7 +3,7 @@ TestFileName=	"ML_TestData.csv"
 ssoKore=		True
 KorePlatform="https://bots.kore.ai"
 USEKORE=True
-USEGOOGLE=True
+USEGOOGLE=False
 USELUIS=False
 subscriptionToken=	""
 Token_DF=		"ecf2caaa9841447ba330d73306f976d7"
@@ -11,3 +11,7 @@ botIdDF=		"15d93a17-4cfa-489d-9058-2f720a88ac7a"
 debug=True
 userIdKore="u-f5047708-1b41-5dfe-960a-64fef9638329"
 authTokenKore="bearer xowAczxCrk4-bSpj3-lQNgcBmGdtIseQxFb6dyFIBZ1cheL6Vdj_1fW-e7R8MgMV"
+koreClientId="cs-d153937f-02de-5e5a-972f-3a2d3dc4d17d"
+koreClientName="test"
+koreClientSecret="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.O8ofkkOdeu7h27BzhrpsdvLqhAWAao8Az3zrrNJo1vI"
+
