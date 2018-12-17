89a9d73718994c57410ae016552f792d27c46aeb
aravindpaduru <aravind.paduru@kore.com>
2018-12-17 17:59:15 +0530

    basic code cleanup

--- createIntent.py
+++ createIntent.py
@@ -9,25 +9,25 @@ from watson import *
 import wit
 
 #Global varibles used for reading input from the csv file
-intents=[]
-utterances=[]
-loginResp=[]
-urlL=[""]
-input2={}
-intentset=[]
-idKore=[]
-LuisIntentId=[]
-KorePublicApi = False
+intents         =   []
+utterances      =   []
+loginResp       =   []
+urlL            =   [""]
+input2          =   {}
+intentset       =   []
+idKore          =   []
+LuisIntentId    =   []
+KorePublicApi   = False
 
 
 def main():
-        fr=open(fileName,'r')
-        reader=csv.reader(fr,delimiter=',')
+        fr      =   open(fileName,'r')
+        reader  =   csv.reader(fr,delimiter=',')
         for row in reader:
                 if len(row)<=0:
                         continue
-                row[0] =row[0].strip().lower().replace("-","").replace("_"," ")
-                if row[0]==None or row[0].strip()=='':
+                row[0]  =   row[0].strip().lower().replace("-","").replace("_"," ")
+                if row[0] == None or row[0].strip() == '':
                         continue
                 while '  ' in row[0]: row[0] =row[0].replace('  ',' ')
                 if not row[1] in utterances:
@@ -45,13 +45,13 @@ def main():
             if ssoKore is False:
                 authTokenKore,userIdKore = loginToKore(KoreEmailId, KorePassword, KorePlatform)
 
-            headersKore['authorization']=authTokenKore #passing the authorization token to the configBot.py file
+            headersKore['authorization'] = authTokenKore #passing the authorization token to the configBot.py file
             botIdKore, dgValue = createKoreBot(botName,userIdKore,authTokenKore,KorePlatform,KorePublicApi)
             print("New bot "+botName+" has been created in Kore with botid: "+ botIdKore)
             prepKore(intentset,intents,utterances,botIdKore,userIdKore,authTokenKore, dgValue)
 
         if USELUIS:
-            botIdLuis=createLuisBot(botName)
+            botIdLuis = createLuisBot(botName)
             print("New bot "+botName+" has been created in Luis with botid: " +botIdLuis)
             prepLuis(intentset,intents,utterances,botIdLuis)
         else:
@@ -60,7 +60,7 @@ def main():
         #For the google platform, we need to send all the training utterances for an intent at once.
         #Calling an empty dictionary to collect all these utterances.
         for i in range(len(intentset)):
-            input2[intentset[i]]=[]
+            input2[intentset[i]] = []
 
         for i in range(len(intents)):
             input2[intents[i]].append(utterances[i])
@@ -77,7 +77,7 @@ def main():
           for j in tqdm(range(len(intentset))):
               WatsonAddIntentAndUtterance(watsonBotId, intentset[j],input2[intentset[j]])
         else:
-          watsonBotId=""
+          watsonBotId = ""
         if USEWIT:
           witSession = requests.Session()
           print("Create Wit bot")
@@ -89,7 +89,7 @@ def main():
               wit.addUtterances(witSession, witBotId, witIntentId, witSemanticTagsId, input2[intentset[j]], intentset[j])
           #print("Adding train utterances")
         else:
-          witBotToken=""
+          witBotToken = ""
 
         print("Creating the config file (testconfig.json) for the runTest.py file.")
         createConfigFile(botName,botIdKore,userIdKore,authTokenKore,KorePlatform,urlL[0],botIdDF,Token_DF,watsonBotId,witBotToken)
@@ -100,18 +100,18 @@ def prepLuis(intentset,intents,utterances,botIdLuis):
             LuisIntentId.append( addLuisIntent(intentset[i],botIdLuis))#Adding Intent in Luis
 
         print("Adding train utterances in Luis")
-        th=[]
+        th = []
         for i in tqdm(range(len(intents))):
             #addLuisUtterance(utterances[i],LuisIntentId[intentset.index(intents[i])],botIdLuis,intents[i])
             th.append(threading.Thread(target=addLuisUtterance,
-                 args=([utterances[i],LuisIntentId[intentset.index(intents[i])],botIdLuis,intents[i]])))
+                 args = ([utterances[i],LuisIntentId[intentset.index(intents[i])],botIdLuis,intents[i]])))
             th[-1].start()
             if not i%10:
                 [thread.join() for thread in th]
                 th.clear()
             
         print("Fetching the endpoint URL to hit, for Luis, to check response by its bot")           
-        urlL[0]= getLuisEndPointUrl(botIdLuis)
+        urlL[0] = getLuisEndPointUrl(botIdLuis)
 
 
 def prepKore(intentset, intents, utterances,botIdKore,userIdKore,authTokenKore, dgValue):
@@ -119,7 +119,7 @@ def prepKore(intentset, intents, utterances,botIdKore,userIdKore,authTokenKore, 
         for i in tqdm(range(len(intentset))):
             if "None" == WatsonCleanIntent(intentset[i]):idKore.append(dgValue)
             else:idKore.append(addIntentKore(intentset[i],botIdKore,userIdKore,authTokenKore,KorePlatform))
-        th=[]
+        th = []
         print("Adding train utterances in Kore")
         addKoreUtterancesBulk(utterances,botIdKore,intents,userIdKore,authTokenKore,KorePlatform)
         print("waiting on intermediate training of the Kore bot to finish")
@@ -128,31 +128,31 @@ def prepKore(intentset, intents, utterances,botIdKore,userIdKore,authTokenKore, 
         trainKore(botIdKore,userIdKore,authTokenKore,KorePlatform)
 
 def createConfigFile(botName,botIdKore,userIdKore,authTokenKore,KorePlatform,urlL,botIdDF,Token_DF,watsonBotId,witBotToken):
-	config= {
-		"botname_Kore":	botName,
-		"uid_Kore":userIdKore,
-		"streamid_Kore":botIdKore,
-		"urlKa":KorePlatform,
-		"KorePublicApi":KorePublicApi,
-		"FileName": TestFileName,
-		"Token_DF":Token_DF,
-		"botname_DF":	botIdDF,
-		"urlL":	urlL,
-		"USEKORE":USEKORE,
-		"USEGOOGLE":USEGOOGLE,
-		"USELUIS":USELUIS,
-		"USEWATSON":USEWATSON,
-		"watsonBotId":watsonBotId,
-		"USEWIT":USEWIT,
-		"witBotToken":witBotToken,
-		"lang":lang,
-		"RESULTSFILE":RESULTSFILE
-		}
+	config = {
+                "botname_Kore" : botName,
+                "uid_Kore"     : userIdKore,
+                "streamid_Kore": botIdKore,
+                "urlKa"        : KorePlatform,
+                "KorePublicApi": KorePublicApi,
+                "FileName"     : TestFileName,
+                "Token_DF"     : Token_DF,
+                "botname_DF"   : botIdDF,
+                "urlL"         : urlL,
+                "USEKORE"      : USEKORE,
+                "USEGOOGLE"    : USEGOOGLE,
+                "USELUIS"      : USELUIS,
+                "USEWATSON"    : USEWATSON,
+                "watsonBotId"  : watsonBotId,
+                "USEWIT"       : USEWIT,
+                "witBotToken"  : witBotToken,
+                "lang"         : lang,
+                "RESULTSFILE"  : RESULTSFILE
+                }
 	if config["KorePublicApi"]:config["token_Kore"] = koreClientSecret
 	else:config["token_Kore"] = authTokenKore
-	f=open("testconfig.json","w")
-	json.dump(config,f, indent=2, ensure_ascii=False,sort_keys=True)
-	f.close()
+	test_config_file = open("testconfig.json","w")
+	json.dump(config,test_config_file, indent = 2, ensure_ascii=False,sort_keys=True)
+	test_config_file.close()
 
 if __name__ == '__main__':
         main()
--- runTest.py
+++ runTest.py
@@ -4,15 +4,16 @@ from threading import Thread
 from tqdm import tqdm
 from watson import *
 from odfhandle import *
-'''Three global arrays declared to get input from the ML csv file to get the utterance, expected task name and the type of utterance and 1 array for the output to capture the matched intent and status(success or failure)'''
-Utterances=[]
-TaskNames=[]
-Types=[]
-outputs=[]
-urlDF="https://console.dialogflow.com/v1/query"
 
-NUM_THREADS=10
-config={}
+'''Three global arrays declared to get input from the ML csv file to get the utterance, expected task name and the type of utterance and 1 array for the output to capture the matched intent and status(success or failure)'''
+Utterances  =   []
+TaskNames   =   []
+Types       =   []
+outputs     =   []
+urlDF       =   "https://console.dialogflow.com/v1/query"
+
+NUM_THREADS =   10
+config      =   {}
 
 def printif(*args):
     return
@@ -22,31 +23,32 @@ def printif(*args):
 #tqdm = lambda x:x
 
 def getJWT(clientSecret): # should be modified, need not be used
-	ID = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
-	dig = hmac.new(clientSecret.encode("utf-8"), msg=ID.encode("utf-8"), digestmod=hashlib.sha256).digest()
-	H=base64.urlsafe_b64encode(dig).decode()
-	return ID+"."+H[:-1]
+	ID  =   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+	dig =   hmac.new(clientSecret.encode("utf-8"), msg=ID.encode("utf-8"), digestmod=hashlib.sha256).digest()
+	H   =   base64.urlsafe_b64encode(dig).decode()
+	return ID + "." + H[:-1]
 
 def find_intent3(sheet,i,ses):
-        MatchedIntents_Kore=['None','Null','Null','Null']
-        MatchedIntents_DF=['None','Null']
-        MatchedIntents_Luis=['None','Null']
-        MatchedIntents_Watson=['None','Null','Null']
+        MatchedIntents_Kore     =   ['None','Null','Null','Null']
+        MatchedIntents_DF       =   ['None','Null']
+        MatchedIntents_Luis     =   ['None','Null']
+        MatchedIntents_Watson   =   ['None','Null','Null']
         output=[]
         #In the output, appending the inputs and matched intents to compare with the expected task name
-        TaskNames[i] = TaskNames[i].replace("\xa0"," ")
+        TaskNames[i]    =    TaskNames[i].replace("\xa0"," ")
         output.append(TaskNames[i])
         output.append(Utterances[i])
         output.append(Types[i])
-        thKORE=Thread(target=callKoreBot,args=(MatchedIntents_Kore,Utterances[i], ses[0]));thKORE.start()
-        thDF=Thread(target=callDFBot,args=(MatchedIntents_DF,Utterances[i],ses[1]));thDF.start()
-        thLUIS=Thread(target=callLUISBot,args=(MatchedIntents_Luis,Utterances[i],ses[2]));thLUIS.start()
-        thWatson=Thread(target=callWatsonBot,args=(MatchedIntents_Watson,Utterances[i],ses[3]));thWatson.start()
+        thKORE  =   Thread(target = callKoreBot  ,args = (MatchedIntents_Kore,Utterances[i]  ,ses[0]));thKORE.start()
+        thDF    =   Thread(target = callDFBot    ,args = (MatchedIntents_DF,Utterances[i]    ,ses[1]));thDF.start()
+        thLUIS  =   Thread(target = callLUISBot  ,args = (MatchedIntents_Luis,Utterances[i]  ,ses[2]));thLUIS.start()
+        thWatson=   Thread(target = callWatsonBot,args = (MatchedIntents_Watson,Utterances[i],ses[3]));thWatson.start()
         thKORE.join();thDF.join();thLUIS.join();thWatson.join()
         output.append(MatchedIntents_Kore[0])
-        if MatchedIntents_Kore[0]==TaskNames[i]:
+
+        if MatchedIntents_Kore[0] == TaskNames[i]:
             output.append('pass')
-        elif MatchedIntents_Kore[0][:10]=='Ambiguity:' and TaskNames[i]=='None':
+        elif MatchedIntents_Kore[0][:10] == 'Ambiguity:' and TaskNames[i] == 'None':
             output.append('pass')
         else:
             output.append('fail')
@@ -54,21 +56,22 @@ def find_intent3(sheet,i,ses):
         output.append(str(MatchedIntents_Kore[2])) # ML score
         output.append(str(MatchedIntents_Kore[3])) # FAQ score
         output.append(MatchedIntents_DF[0])
-        if(MatchedIntents_DF[0]==TaskNames[i]):
+
+        if(MatchedIntents_DF[0] == TaskNames[i]):
             output.append('pass')
         else:
             output.append('fail')
         output.append(str(MatchedIntents_DF[1]))
-
         output.append(MatchedIntents_Luis[0])
-        if(MatchedIntents_Luis[0]==TaskNames[i]):
+
+        if(MatchedIntents_Luis[0] == TaskNames[i]):
                 output.append('pass')
         else:
                 output.append('fail')
         output.append(str(MatchedIntents_Luis[1]))
 
         output.append(MatchedIntents_Watson[0])
-        if(MatchedIntents_Watson[0]==WatsonCleanIntent(TaskNames[i])):
+        if(MatchedIntents_Watson[0] == WatsonCleanIntent(TaskNames[i])):
                 output.append('pass')
         else:
                 output.append('fail')
@@ -80,19 +83,19 @@ def find_intent3(sheet,i,ses):
 
 def main():
     global outputs, config
-    MAP=lambda x,y:list(map(x,y))
-    ses=MAP(lambda x:MAP(lambda y:y(),x),[[requests.session]*4]*NUM_THREADS)
-    th=[None]*NUM_THREADS
-    config=json.load(open(sys.argv[1],"r"))
-    fr=open(config["FileName"],'r')
-    reader=csv.reader(fr,delimiter=',')
-    lines=[line for line in reader][1:]
+    MAP     =   lambda x,y:list(map(x,y))
+    ses     =   MAP(lambda x:MAP(lambda y:y(),x),[[requests.session]*4]*NUM_THREADS)
+    th      =   [None]*NUM_THREADS
+    config  =   json.load(open(sys.argv[1],"r"))
+    fr      =   open(config["FileName"],'r')
+    reader  =   csv.reader(fr,delimiter=',')
+    lines   =   [line for line in reader][1:]
     for row in lines:
         if len(row) <=0:
             continue
         if row[0]==None or row[0].strip()=='':
             continue
-        TaskName = row[0].replace("_"," ").replace("-","").lower()
+        TaskName    = row[0].replace("_"," ").replace("-","").lower()
         if TaskName == "none":TaskName="None"
         Utterances.append(row[1])
         Type = "Positive"
@@ -101,21 +104,24 @@ def main():
         TaskNames.append(TaskName)
     fr.close()
     print("Test data sheet is running")
-    timestr=time.strftime("%d-%m-%Y--%H-%M-%S")
+    timestr =   time.strftime("%d-%m-%Y--%H-%M-%S")
+
     if config.get("RESULTSFILE",""):
-        resultsFileName=config["RESULTSFILE"]
+        resultsFileName = config["RESULTSFILE"]
     else:
-        resultsFileName="ML_Results-"+timestr+".ods"
-        resultsFileName = input("Enter resultsFileName(default:"+resultsFileName+"):")
-        if not resultsFileName:resultsFileName='ML_Results-'+timestr+".ods"
-        if not resultsFileName.split(".")[-1] == "ods": resultsFileName += ".ods"
-    ods = newdoc(doctype='ods', filename=resultsFileName)
-    sheet = Sheet('Results', size=(len(Utterances)+1,18))
-    ods.sheets += sheet
+        resultsFileName = "ML_Results-" + timestr + ".ods"
+        resultsFileName = input("Enter resultsFileName(default:" + resultsFileName +"):")
+        if not resultsFileName:resultsFileName  = 'ML_Results-' + timestr + ".ods"
+        if not resultsFileName.split(".")[-1]   == "ods": resultsFileName += ".ods"
+
+    ods     =   newdoc(doctype='ods', filename=resultsFileName)
+    sheet   =   Sheet('Results', size=(len(Utterances)+1,18))
+    ods.sheets  +=  sheet
     insertRow(sheet,['Expected Task Name','Utterance','Type of Utterance','Matched Intent(s) Kore','Status','Kore Total CS score','Kore ML score','Kore FAQ Score','Matched Intent(s) DF','Status','ScoreDF','Matched Intent(s) Luis','Status','ScoresLuis','Matched Intent Watson ',"Status","ScoreWatson", "RawWatsonResponse"])
     ods.save()
-    outputs = [None]*len(Utterances)
-    prev=0
+    outputs =   [None]*len(Utterances)
+    prev    =   0
+
     for i in tqdm(range(len(Utterances))):
         while len([x for x in th if x]) == NUM_THREADS:
             for x in th:
@@ -130,7 +136,7 @@ def main():
         if j == len(th):input("j==len th!!! How can this be?!")
         th[j] = Thread(target=find_intent3,args=([sheet,i,ses[j]]))
         th[j].start()
-        if i+1==len(Utterances):
+        if i+1 == len(Utterances):
             for x in th:
               if x:x.join()
     ods.save()
@@ -139,18 +145,18 @@ def main():
 def callWatsonBot(MatchedIntents_Watson, input_data, ses):
         if config["USEWATSON"]:
 	        # ses is unused for now.
-	        resp = WatsonFindIntent(config["watsonBotId"], input_data)
+	        resp  =  WatsonFindIntent(config["watsonBotId"], input_data)
 	        MatchedIntents_Watson.clear()
 	        MatchedIntents_Watson.extend([resp[0]["intent"],resp[0]["confidence"], json.dumps(resp)])
 
 def callKoreBot(MatchedIntents_Kore, input_data,ses):
         if config["KorePublicApi"]:
-            jwtToken = getJWT(config["token_Kore"])
-            koreUrl=config["urlKa"]+"/api/v1.1/rest/streams/"+config["streamid_Kore"]+"/findIntent"
-            headers={'auth':jwtToken}
+            jwtToken    =   getJWT(config["token_Kore"])
+            koreUrl     =   config["urlKa"] + "/api/v1.1/rest/streams/" + config["streamid_Kore"] + "/findIntent"
+            headers     =   {'auth':jwtToken}
         else:
-            koreUrl=config["urlKa"]+"/api/1.1/users/"+config["uid_Kore"]+"/builder/streams/"+config["streamid_Kore"]+"/findIntent"
-            headers={'authorization':config["token_Kore"]}
+            koreUrl =   config["urlKa"] + "/api/1.1/users/" + config["uid_Kore"] + "/builder/streams/" + config["streamid_Kore"] + "/findIntent"
+            headers =   {'authorization':config["token_Kore"]}
         respjson={}
         loops = 0
         while(config["USEKORE"]):
@@ -166,7 +172,7 @@ def callKoreBot(MatchedIntents_Kore, input_data,ses):
                     config["token_Kore"] = str(input(resp.text+"\nplease enter new kore token:"))
                     code=1
 
-                  resp=ses.post(koreUrl, headers=headers,json={ "input":input_data,"streamName":config["botname_Kore"]})
+                  resp=ses.post(koreUrl, headers = headers, json = { "input":input_data,"streamName":config["botname_Kore"]})
                   if resp.status_code == 400:
                     matchedIntents_Kore = "None"
                     koreMLScore = "Null"
@@ -193,13 +199,13 @@ def callKoreBot(MatchedIntents_Kore, input_data,ses):
                     if matchedIntents_Kore == rankingObj["intent"].replace("_"," ").lower():
                        rankingMaxObj = rankingObj
                 if "scoring" in rankingMaxObj:
-                    koreMLScore = rankingMaxObj["scoring"].get("mlScore",0.0)
-                    koreCSScore = rankingMaxObj["scoring"].get("score",0.0)
-                    koreFAQScore = rankingMaxObj["scoring"].get("faqScore",0.0)
+                    koreMLScore     =   rankingMaxObj["scoring"].get("mlScore",0.0)
+                    koreCSScore     =   rankingMaxObj["scoring"].get("score",0.0)
+                    koreFAQScore    =   rankingMaxObj["scoring"].get("faqScore",0.0)
                 else:
-                    koreMLScore = rankingMaxObj.get("mlScore", 0.0)
-                    koreCSScore = rankingMaxObj.get("score", 0.0)
-                    koreFAQScore = rankingMaxObj.get("faqScore", 0.0)
+                    koreMLScore     =   rankingMaxObj.get("mlScore", 0.0)
+                    koreCSScore     =   rankingMaxObj.get("score", 0.0)
+                    koreFAQScore    =   rankingMaxObj.get("faqScore", 0.0)
               else:
                 matchedIntents_Kore="Ambiguity:"
                 koreCSScore  = 0.0
@@ -215,25 +221,26 @@ def callKoreBot(MatchedIntents_Kore, input_data,ses):
                 koreFAQScore = 0.0
         else:
             printif("NULL3",input_data)
-            matchedIntents_Kore='None'
-            koreCSScore='Null'
-            koreMLScore='Null'
-            koreFAQScore = 'Null'
-        if(matchedIntents_Kore=='Default Fallback Intent'.lower()):
-                matchedIntents_Kore='None'
+            matchedIntents_Kore =   'None'
+            koreCSScore     = 'Null'
+            koreMLScore     = 'Null'
+            koreFAQScore    = 'Null'
+        if(matchedIntents_Kore == 'Default Fallback Intent'.lower()):
+                matchedIntents_Kore =   'None'
         MatchedIntents_Kore.clear()
         MatchedIntents_Kore.extend([matchedIntents_Kore,koreCSScore,koreMLScore, koreFAQScore])
 
 def callDFBot(MatchedIntents_DF, input_data,ses):
     if config["USEGOOGLE"]:
-        query=urllib.parse.quote(input_data.replace("!",""))
-        params = {
-		"query":query,
-		"lang":"en",
-		"sessionId":config["botname_DF"],
-		"timezone":"UTC"
-		}
+        query   =   urllib.parse.quote(input_data.replace("!",""))
+        params  = {
+                        "query":query,
+                        "lang":"en",
+                        "sessionId":config["botname_DF"],
+                        "timezone":"UTC"
+                    }
         headers = {"authorization": "Bearer "+config["Token_DF"]}
+
         while(1):
             try:
                 response = ses.get( urlDF,  headers=headers,params=params)
@@ -244,19 +251,20 @@ def callDFBot(MatchedIntents_DF, input_data,ses):
                 print("Error while finding intent in google", e)
                 print("GOOGLE","get", urlDF, "headers=",headers,"params",params)
                 time.sleep(1)
-        if not responsejson=={} and ('result' in responsejson) and ('metadata' in responsejson['result']) and ('intentName' in responsejson['result']['metadata']):
-            matchedIntents_DF=responsejson['result']['metadata']['intentName'].lower()
+
+        if not responsejson == {} and ('result' in responsejson) and ('metadata' in responsejson['result']) and ('intentName' in responsejson['result']['metadata']):
+            matchedIntents_DF   =  responsejson['result']['metadata']['intentName'].lower()
             score=responsejson['result']['score']#Getting the confidence score.
-            if(matchedIntents_DF=='Default Fallback Intent'):
-                matchedIntents_DF='None'
+            if(matchedIntents_DF == 'Default Fallback Intent'):
+                matchedIntents_DF  = 'None'
         else:
-            matchedIntents_DF='None'
-            score='null'
+            matchedIntents_DF   =  'None'
+            score   = 'null'
             print("null score google")
             print("GOOGLE","get", urlDF, "headers=",headers,"params",params)
     else:
-        matchedIntents_DF='None'
-        score=0.1
+        matchedIntents_DF = 'None'
+        score = 0.1
     MatchedIntents_DF.clear()
     MatchedIntents_DF.extend([matchedIntents_DF,score])
             
@@ -264,26 +272,27 @@ def callLUISBot(MatchedIntents_Luis,input_data,ses):
     if config["USELUIS"]:
         while(1):
             try:
-                respLuis=ses.get(config["urlL"]+input_data)#Reading the JSON response for luis.ai
+                respLuis = ses.get(config["urlL"] + input_data)#Reading the JSON response for luis.ai
                 respLuis.raise_for_status()
-                respluis=respLuis.json()
+                respluis = respLuis.json()
                 break
             except Exception as e:
                 print("Error while finding intent in Luis", e)
                 time.sleep(1)
+
         if respluis!={} and ('topScoringIntent' in respluis) and ('intent' in respluis['topScoringIntent']):
-                matchedIntents_Luis=respluis['topScoringIntent']['intent'].lower()#Luis Output Taken Here
-                score=respluis['topScoringIntent']['score']#Getting Luis Score
+                matchedIntents_Luis = respluis['topScoringIntent']['intent'].lower()#Luis Output Taken Here
+                score   = respluis['topScoringIntent']['score']#Getting Luis Score
                 if respluis['topScoringIntent']['intent']=='None':
-                        matchedIntents_Luis='None'
+                        matchedIntents_Luis = 'None'
         else:
-                matchedIntents_Luis='None'
-                score='Null'
+                matchedIntents_Luis = 'None'
+                score = 'Null'
                 print("LUIS","get" , (config["urlL"]+input_data))
                 print(respLuis)
     else:
-        matchedIntents_Luis='None'
-        score=0.1
+        matchedIntents_Luis = 'None'
+        score = 0.1
     MatchedIntents_Luis.clear()
     MatchedIntents_Luis.extend([matchedIntents_Luis,score])
 
@@ -291,9 +300,9 @@ if __name__ == "__main__":
     if len(sys.argv) !=2:
         print("Usage: python read.py <testconfig file>")
         exit()
-    start_time=time.time()
-    resultsFileName=main()
-    print(time.time()-start_time)
+    start_time      = time.time()
+    resultsFileName = main()
+    print(time.time() - start_time)
     print(resultsFileName)
     ods = opendoc(filename=resultsFileName)
     tabulate.main(ods)
