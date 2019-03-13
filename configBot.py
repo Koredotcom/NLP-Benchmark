9f680eff72d30604a4166710f941af032f8cbff6
vishnune-kore <vishnu.nekkanti@kore.com>
2019-03-13 19:33:39 +0530

    updated config bot pipeline

--- configBot.py
+++ configBot.py
@@ -1,36 +1,42 @@
-fileName = "ML_Train.csv"
-TestFileName = "ML_TestData.csv"
-RESULTSFILE = ""
-debug = True
-botName = "BankBot"
-lang = "en"
+import json
+
+
+with open("configBot.json", "r") as fp:
+	conf = json.load(fp)
+
+fileName =  conf["fileName"]  #conf["ML_Train.csv"]
+TestFileName =  conf["TestFileName"]  #"ML_TestData.csv"
+RESULTSFILE =  conf["RESULTSFILE"]  #""
+debug =  conf.get("debug")  #True
+botName =  conf["botName"]  #"BankBot"
+lang =  conf["lang"]  #"en"
 
 # Platforms to enable
-USEKORE = True
-USEGOOGLE = False
-USELUIS = False
-USEWATSON = False
-USEWIT = False
+USEKORE =  conf["USEKORE"]  #True
+USEGOOGLE =  conf["USEGOOGLE"]  #False
+USELUIS =  conf["USELUIS"]  #False
+USEWATSON =  conf["USEWATSON"]  #False
+USEWIT =  conf["USEWIT"]  #False
 
 # Kore.ai
-KorePlatform = "https://bots.kore.ai"
-ssoKore=True
-userIdKore="u-f5047708-1b41-5dfe-960a-64fef9638329"
-authTokenKore="bearer xowAczxCrk4-bSpj3-lQNgcBmGdtIseQxFb6dyFIBZ1cheL6Vdj_1fW-e7R8MgMV"
-KoreEmailId=""
-KorePassword=""
+KorePlatform =  conf["KorePlatform"]  #"https://bots.kore.ai"
+ssoKore = conf["ssoKore"]
+userIdKore = conf["userIdKore"] 
+authTokenKore = conf["authTokenKore"] 
+KoreEmailId = conf["KoreEmailId"]  #
+KorePassword = conf["KorePassword"]  #
 # optional platforms to enable to compare with kore
 # Luis.ai
-subscriptionToken=""
+subscriptionToken= conf["subscriptionToken"]  #
 
 # DialogFlow.ai
-Token_DF = "ecf2caaa9841447ba330d73306f976d7"
-botIdDF = "15d93a17-4cfa-489d-9058-2f720a88ac7a"
+Token_DF =  conf["Token_DF"]  
+botIdDF =  conf["botIdDF"]  
 
 # Watson.ai
-watson_uid = "d5124i63-98e1-4726-bc6e-3015a7ec3d57"
-watson_passwd = "EKhenaFaQoNg"
+watson_uid =  conf["watson_uid"]  
+watson_passwd =  conf["watson_passwd"]  
 
 # Wit.ai
-witUserToken = "Bearer VZZRDXK4BOD6GPLBESFQH3HPSOXMI76W"
+witUserToken =  conf["witUserToken"]  
 
--- getParams.py
+++ getParams.py
@@ -76,37 +76,38 @@ else:
 	watson_uid = ""
 	watson_passwd = ""
 
-fileName="ML_Train.csv"
-TestFileName = "ML_TestData.csv"
+fileName = input("Training file name?[default is \"ML_Train.csv\"] ").lower().strip()
+TestFileName = input("Training file name?[default is \"ML_TestData.csv\"] ").lower().strip()
+fileName="ML_Train.csv" if fileName == "" else fileName
+TestFileName = "ML_TestData.csv" if TestFileName == "" else TestFileName
 
 botName = input("Please enter name of the bot you want to create(default:BankBot):")
 if not botName:botName = "BankBot"
 
-fr=open('configBot.py','w')
-fr.write("fileName=	\""+fileName+"\"\n")
-fr.write("TestFileName=	\""+TestFileName+"\"\n")
-fr.write("ssoKore=		"+str(ssoKore)+"\n")
-fr.write("KorePlatform=\""+str(KorePlatform)+"\"\n")
-fr.write("USEKORE="+str(USEKORE)+"\n")
-fr.write("USEGOOGLE="+str(USEDF)+"\n")
-fr.write("USEWATSON="+str(USEWATSON)+"\n")
-fr.write("USELUIS="+str(USELUIS)+"\n")
-fr.write("USEWIT="+str("False")+"\n")
-fr.write("subscriptionToken=	\""+subscriptionToken+"\"\n")
-fr.write("Token_DF=		\""+Token_DF+"\"\n")
-fr.write("botIdDF=		\""+botIdDF+"\"\n")
-if userIdKore:
-	fr.write("userIdKore=		\""+userIdKore+"\"\n")
-if authTokenKore:
-	fr.write("authTokenKore=	\""+	authTokenKore+"\"\n")
-if KoreEmailId:
-	fr.write("KoreEmailId=	\""+	KoreEmailId+"\"\n")
-if KorePassword:
-	fr.write("KorePassword=	\""+	KorePassword+"\"\n")
-fr.write("witUserToken=	\""+	str(witUserToken)+"\"\n")
-fr.write("watson_uid=	\""+	str(watson_uid)+"\"\n")
-fr.write("watson_passwd=\""+	str(watson_passwd)+"\"\n")
-fr.write("botName=	\""+	str(botName)+"\"\n")
-fr.write("RESULTSFILE= 	\""+"testconfig.json"+"\"\n")
-fr.write("lang=	\""+	str("en")+"\"\n")
+conf ={
+	"fileName":fileName,
+	"TestFileName":TestFileName,
+	"ssoKore":ssoKore,
+	"KorePlatform":KorePlatform,
+	"USEKORE":USEKORE,
+	"USEGOOGLE":USEDF,
+	"USEWATSON":USEWATSON,
+	"USELUIS":USELUIS,
+	"USEWIT":False,        # ?? why       
+	"subscriptionToken":subscriptionToken,
+	"Token_DF":Token_DF,
+	"botIdDF":botIdDF,
+	"userIdKore":userIdKore,
+	"authTokenKore":authTokenKore,
+	"KoreEmailId":KoreEmailId,
+	"KorePassword":KorePassword,
+	"witUserToken":witUserToken,
+	"watson_uid":watson_uid,
+	"watson_passwd":watson_passwd,
+	"botName":botName,
+	"RESULTSFILE":"",
+	"lang":"en"
+}
 
+with open("configBot.json", "w") as fp:
+	json.dump(conf, fp, indent=4)
